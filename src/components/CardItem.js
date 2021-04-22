01ea13acabcfeb0933fd678de6becf40cd05b99f
David <davidpavlovicdev@gmail.com>
2021-04-22 14:23:54 +0200

    add font, update gitignore

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,12 +1,8 @@
 function CardItem(props) {
-    const styleObj = {
-        color: props.cardColor,
-    }
-
     return(
         <figure id={props.cardIndex} className="card__item">
             <img src={props.cardItem} alt={`turtle ` + props.cardNickname}/>
-            <figcaption style={styleObj}>This is {props.cardName}</figcaption>
+            <figcaption style={{color: props.cardColor}}>This is {props.cardName}</figcaption>
         </figure>
     );
 }
